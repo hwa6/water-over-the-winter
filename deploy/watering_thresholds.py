a4771357613041b77b485f98001ea63d649a2733
hwa6 <handerson5486@gmail>
2022-05-30 23:52:28 -0400

    added GET threshold functionality

--- deploy/watering_thresholds.py
+++ deploy/watering_thresholds.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 ##This file will be responsible for using HTTP GET to get the thresholds for each plant
 ##The front end application allows the user to update when they want the plant to be watered, this ensures our main loop is updated to the latest change 
+
+from pymongo import MongoClient
+from pprint import pprint
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGODB_URI = (os.environ['MONGODB_URI'])
+
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient(MONGODB_URI)
+db=client.wateroverthewinter
+
+def get():
+    thresholds = [0]*4;
+    for i in range(0,4):
+        result = db.plants.find_one({'id': i})
+        thresholds[i] = result.get('threshold')
+    return thresholds
--- testing/backend_test.py
+++ testing/backend_test.py
@@ -12,14 +12,11 @@ MONGODB_URI = (os.environ['MONGODB_URI'])
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient(MONGODB_URI)
 db=client.wateroverthewinter
-## Issue the serverStatus command and print the results
-##serverStatusResult=db.command("serverStatus")
-##pprint(serverStatusResult)
 result = db.plants.find_one({'id': 0})
 pprint(result)
 
 db.plants.update_one({'id': 0}, {'$set':{'moisture_level':500}})
 result = db.plants.find_one({'id': 0})
-pprint(result)
+print(result.get('moisture_level'))
 
 db.plants.update_one({'id': 0}, {'$set':{'moisture_level':1000}})
