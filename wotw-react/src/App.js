b9523dedc11a922b81f91b32c6b4470201bbd5a8
Henry Anderson <handerson5486@gmail.com>
2022-06-05 13:30:18 -0400

    improvements to UI

--- wotw-react/src/App.js
+++ wotw-react/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import pottedplant from './images/pottedplant.png';
+import cactus from './images/cactus.png';
+import flower from './images/flower.png';
+import palmtree from './images/palmtree.png';
 import './App.css';
 
 function App() {
@@ -22,42 +25,51 @@ function App() {
             Update Threshold
           </button>
         </div>
-        <div className="Card">
-          <h1 className="PlantTitle">Plant 1</h1>
-          <p1>Current Moisture Reading: </p1>
-          <p1>Current Moisture Threshold: </p1>
-          <div>
-            <p1>New Threshold Value: </p1>
-            <input></input>
-          </div>
 
-          <button className="Button" onClick={clickedButton}>
+        <div className="Card">
+          <div className="Plant-image-holder">
+            <img className="Plant-image" src={cactus}></img>
+          </div>
+          <h1 className="Card-title-text">Plant 2</h1>
+          <p1 className="Card-body-text">Current Moisture Reading: </p1>
+          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
+          <div>
+            <p1 className="Card-body-text">New Threshold: </p1>
+            <input className="Card-input"></input>
+          </div>
+          <button className="Card-update-button" onClick={clickedButton}>
             Update Threshold
           </button>
         </div>
-        <div className="Card">
-          <h1 className="PlantTitle">Plant 1</h1>
-          <p1>Current Moisture Reading: </p1>
-          <p1>Current Moisture Threshold: </p1>
-          <div>
-            <p1>New Threshold Value: </p1>
-            <input></input>
-          </div>
 
-          <button className="Button" onClick={clickedButton}>
+        <div className="Card">
+          <div className="Plant-image-holder">
+            <img className="Plant-image" src={flower}></img>
+          </div>
+          <h1 className="Card-title-text">Plant 3</h1>
+          <p1 className="Card-body-text">Current Moisture Reading: </p1>
+          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
+          <div>
+            <p1 className="Card-body-text">New Threshold: </p1>
+            <input className="Card-input"></input>
+          </div>
+          <button className="Card-update-button" onClick={clickedButton}>
             Update Threshold
           </button>
         </div>
-        <div className="Card">
-          <h1 className="PlantTitle">Plant 1</h1>
-          <p1>Current Moisture Reading: </p1>
-          <p1>Current Moisture Threshold: </p1>
-          <div>
-            <p1>New Threshold Value: </p1>
-            <input></input>
-          </div>
 
-          <button className="Button" onClick={clickedButton}>
+        <div className="Card">
+          <div className="Plant-image-holder">
+            <img className="Plant-image" src={palmtree}></img>
+          </div>
+          <h1 className="Card-title-text">Plant 4</h1>
+          <p1 className="Card-body-text">Current Moisture Reading: </p1>
+          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
+          <div>
+            <p1 className="Card-body-text">New Threshold: </p1>
+            <input className="Card-input"></input>
+          </div>
+          <button className="Card-update-button" onClick={clickedButton}>
             Update Threshold
           </button>
         </div>
