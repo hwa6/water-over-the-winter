db38667339c8049071c15c38b32402fbc0df6eb5
hwa6 <handerson5486@gmail>
2022-06-05 00:46:06 -0400

    testing multiple sensor at once

--- testing/adc_test.py
+++ testing/adc_test.py
@@ -6,22 +6,36 @@ import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+def read(sensor_id):
 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
+    # create the spi bus
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+    # create the cs (chip select)
+    cs = digitalio.DigitalInOut(board.D5)
 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+    # create the mcp object
+    mcp = MCP.MCP3008(spi, cs)
 
-print('Raw ADC Value: ', chan.value)
-print('ADC Voltage: ' + str(chan.voltage) + 'V')
-high = 2.8
-low = 1.7
-adjusted_val = (chan.voltage-low) / (high-low) * 100
-normalized_val = 100 - abs(adjusted_val)
-print(normalized_val)
+    if sensor_id == 1:    
+        # create an analog input channel on pin 0
+        chan = AnalogIn(mcp, MCP.P0)
+    elif sensor_id == 2:
+        # create an analog input channel on pin 0
+        chan = AnalogIn(mcp, MCP.P1)
+    elif sensor_id == 3:
+        # create an analog input channel on pin 0
+        chan = AnalogIn(mcp, MCP.P2)
+    elif sensor_id == 4:
+        # create an analog input channel on pin 0
+        chan = AnalogIn(mcp, MCP.P3)
+    else:
+        print('Invalid sensor number provided.')
+
+    print('Raw ADC Value: ', chan.value)
+    print('ADC Voltage: ' + str(chan.voltage) + 'V')
+    high = 2.8
+    low = 1.7
+    adjusted_val = (chan.voltage-low) / (high-low) * 100
+    normalized_val = 100 - abs(adjusted_val)
+    print(normalized_val)
