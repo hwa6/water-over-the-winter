701aba7f1b3a901486272732729e9156d5f9e8d3
hwa6 <handerson5486@gmail>
2022-06-05 01:25:30 -0400

    tweaks to deployment code

--- deploy/main.py
+++ deploy/main.py
@@ -5,49 +5,27 @@ import moisture_levels
 import watering_thresholds
 import logs
 
-#Threshold moisture value
-threshold = 50
+#Duration of watering burst
+interval = 1
 
 ##Configuring board numbering scheme
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
+#Assiging pin values
+#Pins for water pumps
+pump_pin_A = 32
+pump_pin_B = 36
+pump_pin_C = 38
+pump_pin_D = 40
+pump_pins = [32,36,38,40]
 
-##Assiging pin values
-
-##Pin for water pump
-pump_pin = 29
-
-##Pins for moisture sensors
-moisture_pin_A = 31
-moisture_pin_B = 33
-moisture_pin_C = 35
-moisture_pin_D = 37
-moisture_pins = [31,33,35,37]
-
-#Pins for solenoid valves
-valve_pin_A = 32
-valve_pin_B = 36
-valve_pin_C = 38
-valve_pin_D = 40
-valve_pins = [32,36,38,40]
-
-##Configuring GPIO pin types
-
-##Water pump is output
-GPIO.setup(pump_pin, GPIO.OUT, initial=GPIO.LOW)
-
-##Moisture sensors are inputs
-GPIO.setup(moisture_pin_A, GPIO.IN)
-GPIO.setup(moisture_pin_B, GPIO.IN)
-GPIO.setup(moisture_pin_C, GPIO.IN)
-GPIO.setup(moisture_pin_D, GPIO.IN)
-
-##Solenoid valves are outputs
-GPIO.setup(valve_pin_A, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(valve_pin_B, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(valve_pin_C, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(valve_pin_D, GPIO.OUT, initial=GPIO.LOW)
+#Configuring GPIO pin types
+#Water pumps are outputs
+GPIO.setup(pump_pin_A, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pump_pin_B, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pump_pin_C, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pump_pin_D, GPIO.OUT, initial=GPIO.LOW)
 
 #main loop
 while True:
@@ -58,7 +36,7 @@ while True:
 
     print('Reading from moisture sensors')
     for i in range (0,4):
-        moisture_readings[i] = GPIO.input((moisture_pins[i]))
+        moisture_readings[i] = sensors.read(i)
     print(moisture_readings)
     print('Updating database with sensor readings')
     moisture_levels.update(moisture_readings)
@@ -66,14 +44,16 @@ while True:
     thresholds = watering_thresholds.get()
     print(thresholds)
     print("Determining if water dispensation is neccesary")
-    moisture_readings = [48 , 53, 52, 50]
-    thresholds = [60, 40, 40, 40]
+    #moisture_readings = [48 , 53, 52, 50]
+    #thresholds = [60, 40, 40, 40]
     for i in range (0,4):
         if(moisture_readings[i]<thresholds[i]):
             log_needed = True
             watered_plants.append(i)
             print('Watering plant {0}'.format(i+1))
-            GPIO.output(valve_pins[i], GPIO.HIGH)
+            GPIO.output(pump_pins[i], GPIO.HIGH)
+            time.sleep(interval)
+            GPIO.output(pump_pins[i], GPIO.LOW)
     if(log_needed):
         logs.log(watered_plants)
     else:
--- deploy/sensors.py
+++ deploy/sensors.py
@@ -7,7 +7,6 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 def read(sensor_id):
-
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -17,16 +16,16 @@ def read(sensor_id):
     # create the mcp object
     mcp = MCP.MCP3008(spi, cs)
 
-    if sensor_id == 1:    
+    if sensor_id == 0:    
         # create an analog input channel on pin 0
         chan = AnalogIn(mcp, MCP.P0)
-    elif sensor_id == 2:
+    elif sensor_id == 1:
         # create an analog input channel on pin 0
         chan = AnalogIn(mcp, MCP.P1)
-    elif sensor_id == 3:
+    elif sensor_id == 2:
         # create an analog input channel on pin 0
         chan = AnalogIn(mcp, MCP.P2)
-    elif sensor_id == 4:
+    elif sensor_id == 3:
         # create an analog input channel on pin 0
         chan = AnalogIn(mcp, MCP.P3)
     else:
@@ -42,12 +41,4 @@ def read(sensor_id):
         normalized_val = 0.00
     if(normalized_val >100):
         normalized_val = 100.00
-    if(chan.voltage<1):
-        print('Error. Are you sure sensor is connected properly?')
-    else:
-        print(normalized_val)
-if __name__ == "__main__":
-    read(1)
-    read(2)
-    read(3)
-    read(4)
+    return normalized_val
