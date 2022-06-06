8aa5c4488168d27300a0bb04f21abf5aa2e8714b
Henry Anderson <handerson5486@gmail.com>
2022-06-06 16:13:27 -0400

    api implements PUT for threshold

--- web/middleware/app.js
+++ web/middleware/app.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var plantAPIRouter = require('./routes/plantAPI');
+var apiRouter = require('./routes/api');
 
 //dotenv.config({ path: '.env.example' });
 dotenv.config({ path: '.env' });
@@ -48,7 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/plants', plantAPIRouter);
+app.use('/plants', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ web/middleware/routes/api.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const { Plant } = require('../models/Models');
+const { Log } = require('../models/Models');
+
+//get all plants. return JSON data
+router.get('/', function (req, res, next) {
+  Plant.find({}, { _id: 0, __v: 0 }, function (err, allPlants) {
+    if (err) {
+      return res.status(400).send('Error with get');
+    } else {
+      res.status(201).send(allPlants);
+    }
+  });
+});
+
+//update a plant threshold.
+router.put('/:id', function (req, res, next) {
+  //find plant with desired id
+  Plant.find({ id: req.params.id }, function (err, searchResult) {
+    if (searchResult[0] === undefined) {
+      res
+        .status(400)
+        .send(
+          'Plant ID number is invalid. Acceptable inputs are 0, 1, 2, and 3.'
+        );
+    } else if (req.body.threshold < 0 || req.body.threshold > 100) {
+      res
+        .status(400)
+        .send('Threshold value is invalid. Acceptable inputs are 0-100.');
+    } else {
+      let filter = { id: req.params.id };
+      let update = {};
+      update.threshold = req.body.threshold;
+      Plant.findOneAndUpdate(filter, update, function (err, success) {
+        if (err) {
+          res.status(400).send('Error updating');
+        } else {
+          res.status(201).send('Successful Update');
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
