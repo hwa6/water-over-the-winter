19e0d9d3d9fbfb459bc873e55d82fd6afb101a5c
Henry Anderson <handerson5486@gmail.com>
2022-06-06 15:29:14 -0400

    fetching settings works

--- web/frontend/src/components/Card.js
+++ web/frontend/src/components/Card.js
@@ -12,8 +12,12 @@ class Card extends React.Component {
           ></img>
         </div>
         <h1 className="Card-title-text">Plant {this.props.plantID}</h1>
-        <p1 className="Card-body-text">Current Moisture Reading: </p1>
-        <p1 className="Card-body-text">Current Moisture Threshold: </p1>
+        <p1 className="Card-body-text">
+          Current Moisture Reading: {this.props.moistureLevel}{' '}
+        </p1>
+        <p1 className="Card-body-text">
+          Current Moisture Threshold: {this.props.moistureThreshold}{' '}
+        </p1>
         <div>
           <p1 className="Card-body-text">New Threshold: </p1>
           <input className="Card-input"></input>
--- web/frontend/src/components/CardHolder.js
+++ web/frontend/src/components/CardHolder.js
@@ -11,23 +11,59 @@ import palmtree from '../images/palmtree.png';
 class CardHolder extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: '' };
+    this.state = {
+      plant1: {},
+      plant2: {},
+      plant3: {},
+      plant4: {},
+    };
   }
   componentDidMount() {
+    const that = this;
     fetch('http://localhost:9000/plants')
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }));
-    console.log(this.state.apiResponse);
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        that.setState({
+          plant1: jsonData[0],
+          plant2: jsonData[1],
+          plant3: jsonData[2],
+          plant4: jsonData[3],
+        });
+        console.log(JSON.stringify(jsonData));
+      });
   }
 
   render() {
     return (
       <div className="CardHolder">
         <h1 className=".Card-body-text">{this.state.apiResponse}</h1>
-        <Card plantID="1" plantPhoto={pottedplant}></Card>
-        <Card plantID="2" plantPhoto={cactus}></Card>
-        <Card plantID="3" plantPhoto={flower}></Card>
-        <Card plantID="4" plantPhoto={palmtree}></Card>
+        <h1 className=".Card-body-text">{this.state.otherResponse}</h1>
+        <Card
+          plantID="1"
+          plantPhoto={pottedplant}
+          moistureLevel={this.state.plant1.moisture_level}
+          moistureThreshold={this.state.plant1.threshold}
+        ></Card>
+        <Card
+          plantID="2"
+          plantPhoto={cactus}
+          moistureLevel={this.state.plant2.moisture_level}
+          moistureThreshold={this.state.plant2.threshold}
+        ></Card>
+        <Card
+          plantID="3"
+          plantPhoto={flower}
+          moistureLevel={this.state.plant3.moisture_level}
+          moistureThreshold={this.state.plant3.threshold}
+        ></Card>
+        <Card
+          plantID="4"
+          plantPhoto={palmtree}
+          moistureLevel={this.state.plant4.moisture_level}
+          moistureThreshold={this.state.plant4.threshold}
+        ></Card>
       </div>
     );
   }
