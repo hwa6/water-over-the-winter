9c38ee008f9f28563f509a0255de65dfc6dc55f5
Henry Anderson <handerson5486@gmail.com>
2022-06-05 21:18:31 -0400

    fully implemented static UI!

--- wotw-react/src/App.js
+++ wotw-react/src/App.js
@@ -1,84 +1,85 @@
-import logo from './logo.svg';
 import pottedplant from './images/pottedplant.png';
 import cactus from './images/cactus.png';
 import flower from './images/flower.png';
 import palmtree from './images/palmtree.png';
+import plant from './images/plant.png';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header"></div>
-      <div className="CardHolder">
-        <div className="Card">
-          <div className="Plant-image-holder">
-            <img className="Plant-image" src={pottedplant}></img>
-          </div>
-          <h1 className="Card-title-text">Plant 1</h1>
-          <p1 className="Card-body-text">Current Moisture Reading: </p1>
-          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
-          <div>
-            <p1 className="Card-body-text">New Threshold: </p1>
-            <input className="Card-input"></input>
-          </div>
-          <button className="Card-update-button" onClick={clickedButton}>
-            Update Threshold
-          </button>
-        </div>
+      <Header></Header>
+      <CenterContent></CenterContent>
+      <Footer></Footer>
+    </div>
+  );
+}
 
-        <div className="Card">
-          <div className="Plant-image-holder">
-            <img className="Plant-image" src={cactus}></img>
-          </div>
-          <h1 className="Card-title-text">Plant 2</h1>
-          <p1 className="Card-body-text">Current Moisture Reading: </p1>
-          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
-          <div>
-            <p1 className="Card-body-text">New Threshold: </p1>
-            <input className="Card-input"></input>
-          </div>
-          <button className="Card-update-button" onClick={clickedButton}>
-            Update Threshold
-          </button>
-        </div>
-
-        <div className="Card">
-          <div className="Plant-image-holder">
-            <img className="Plant-image" src={flower}></img>
-          </div>
-          <h1 className="Card-title-text">Plant 3</h1>
-          <p1 className="Card-body-text">Current Moisture Reading: </p1>
-          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
-          <div>
-            <p1 className="Card-body-text">New Threshold: </p1>
-            <input className="Card-input"></input>
-          </div>
-          <button className="Card-update-button" onClick={clickedButton}>
-            Update Threshold
-          </button>
-        </div>
-
-        <div className="Card">
-          <div className="Plant-image-holder">
-            <img className="Plant-image" src={palmtree}></img>
-          </div>
-          <h1 className="Card-title-text">Plant 4</h1>
-          <p1 className="Card-body-text">Current Moisture Reading: </p1>
-          <p1 className="Card-body-text">Current Moisture Threshold: </p1>
-          <div>
-            <p1 className="Card-body-text">New Threshold: </p1>
-            <input className="Card-input"></input>
-          </div>
-          <button className="Card-update-button" onClick={clickedButton}>
-            Update Threshold
-          </button>
-        </div>
+function PlantCard(props) {
+  return (
+    <div className="Card">
+      <div className="Plant-image-holder">
+        <img className="Plant-image" src={props.plantPhoto}></img>
       </div>
-      <div className="App-footer">
-        <button className="SourceCode-button" onClick={sourceCode}>
-          Source Code
-        </button>
+      <h1 className="Card-title-text">Plant {props.plantID}</h1>
+      <p1 className="Card-body-text">Current Moisture Reading: </p1>
+      <p1 className="Card-body-text">Current Moisture Threshold: </p1>
+      <div>
+        <p1 className="Card-body-text">New Threshold: </p1>
+        <input className="Card-input"></input>
       </div>
+      <button className="App-button-update" onClick={clickedButton}>
+        Update Threshold
+      </button>
+    </div>
+  );
+}
+
+function CardHolder(props) {
+  return (
+    <div className="CardHolder">
+      <PlantCard plantID="1" plantPhoto={pottedplant}></PlantCard>
+      <PlantCard plantID="2" plantPhoto={cactus}></PlantCard>
+      <PlantCard plantID="3" plantPhoto={flower}></PlantCard>
+      <PlantCard plantID="4" plantPhoto={palmtree}></PlantCard>
+    </div>
+  );
+}
+
+function PageTitle(props) {
+  return (
+    <div className="App-titleholder">
+      <h1 className="App-title">Water Over The Winter</h1>
+      <h3 className="App-subtitle">Configure and read moisture levels here!</h3>
+    </div>
+  );
+}
+
+function Header(props) {
+  return (
+    <div className="App-header">
+      <img className="App-logo" src={plant}></img>
+      <button className="App-button-logs">Logs</button>
+    </div>
+  );
+}
+
+function CenterContent(props) {
+  return (
+    <div className="App-centercontent">
+      <PageTitle></PageTitle>
+      <CardHolder></CardHolder>
+    </div>
+  );
+}
+
+function Footer(props) {
+  return (
+    <div className="App-footer">
+      <button className="App-button-sourcecode" onClick={sourceCode}>
+        Source Code
+      </button>
     </div>
   );
 }
@@ -88,7 +89,7 @@ function clickedButton() {
 }
 
 function sourceCode() {
-  console.log('Redirecting to Github Repo');
+  window.location.href = 'https://github.com/hwa6/water-over-the-winter';
 }
 
 export default App;
