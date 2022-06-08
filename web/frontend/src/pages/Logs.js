d343614951cd7d1460de564ace9e2588b88e5afc
Henry Anderson <handerson5486@gmail.com>
2022-06-08 13:58:58 -0400

    router dom start

---
+++ web/frontend/src/pages/Logs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Logs() {
+  return (
+    <Link to="/logs">
+      <div>Hello world!</div>
+    </Link>
+  );
+}
+
+export default Logs;
