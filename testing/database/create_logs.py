99ddca40ffcfd1e5501581d3146bb9e1e1da849d
hwa6 <handerson5486@gmail>
2022-06-14 12:24:14 -0400

    added test for creating dummy logs

---
+++ testing/database/create_logs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from datetime import datetime
+
+MONGODB_URI = (os.environ['MONGODB_URI'])
+client = MongoClient(MONGODB_URI)
+db=client.wateroverthewinter
+
+for i in range (0,10):
+    log = {
+        'message' : "Watered plant " + str(i),
+        'date' : datetime.now().isoformat(),
+    }
+    result = db.logs.insert_one(log)
+print("Created 10 dummy logs.")
