9fcce47f6d84db62620403075d56ae55565d16e1
Henry Anderson <handerson5486@gmail.com>
2022-06-14 13:07:27 -0400

    removed useless footer constructor

--- web/frontend/src/components/Footer.js
+++ web/frontend/src/components/Footer.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="App-footer">
