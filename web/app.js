2f2d606de176b5d5f851e99b91019c1857cc31dd
Henry Anderson <handerson5486@gmail.com>
2022-06-14 20:02:11 -0400

    i think we got it...

--- web/app.js
+++ web/app.js
@@ -46,10 +46,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/plants', plantsRouter);
 app.use('/logs', logsRouter);
 app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
