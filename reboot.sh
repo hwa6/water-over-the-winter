7a9e7cbe3db3a24966d58c613f2d82efdf2da017
Henry Anderson <handerson5486@gmail.com>
2022-06-25 12:46:26 -0400

    functional reboot script

--- reboot.sh
+++ reboot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo 'Checking for potential update'
+echo 'Checking for potential update...'
 if git status; then
     echo 'Update needed. Rebooting.'
+else 
+    echo 'No update detected.'
 fi
