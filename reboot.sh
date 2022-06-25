16e5f4c7d0bc1312b165a96dc4b07a015d53b17a
Henry Anderson <handerson5486@gmail.com>
2022-06-25 13:28:11 -0400

    finalized reboot script. may need to add cd

--- reboot.sh
+++ reboot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo 'Checking for potential update...'
-if git status; then
+if git status -sb | grep 'behind'; then
     echo 'Update needed. Rebooting.'
+    sudo reboot
 else 
     echo 'No update detected.'
 fi
