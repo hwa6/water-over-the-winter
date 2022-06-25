3330547704b2655cb2f3c6da9f658382998e6756
Henry Anderson <handerson5486@gmail.com>
2022-06-25 10:54:05 -0400

    replaced frequency file w config

--- deploy/config.py
+++ deploy/config.py
@@ -12,9 +12,12 @@ MONGODB_URI = (os.environ['MONGODB_URI'])
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient(MONGODB_URI)
 db=client.wateroverthewinter
+result = db.config.find_one()
 
-def read():
-    result = db.config.find_one()
+def frequency():
     print('Frequency is every {0} minutes.'.format(result.get('frequency')))
     return result.get('frequency')
 
+def duration():
+    print('Duration is {0} seconds.'.format(result.get('duration')))
+    return result.get('duration')
